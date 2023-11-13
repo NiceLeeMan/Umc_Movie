0ba3187cf45939447005312a99f205de07fcf5cb
Tom <137988673+NiceLeeMan@users.noreply.github.com>
2023-11-13 22:10:49 +0900

    Create Movies.jsx

---
+++ 6week/pages/Movies.jsx
@@ -0,0 +1,31 @@
+import Movie from "../components/Movie"
+import { movies } from "../movieDummy";
+import { AppContainer } from "../components/Movie.style";
+
+
+function Movies(){
+    return(
+    <>
+    <AppContainer>
+    {
+      movies.results.map(item => {
+
+        return (
+          <Movie
+            key={item.id}
+            title = {item.title}
+            poster_path = {item.poster_path} 
+            vote_average = {item.vote_average}
+            overview = {item.overview}
+          />
+          
+        )
+      })
+    }
+    </AppContainer>
+
+    </>
+    )
+}
+
+export default Movies
