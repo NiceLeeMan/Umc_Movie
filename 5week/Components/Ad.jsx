fc1723a84eeff73dba1acabed30ee6ceafdb7ff9
Tom <137988673+NiceLeeMan@users.noreply.github.com>
2023-11-13 20:03:41 +0900

    Update Ad.jsx

--- 5week/Components/Ad.jsx
+++ 5week/Components/Ad.jsx
@@ -1,1 +1,18 @@
+import styled from "styled-components";
+import UMC from '../UMC.svg';
 
+const Img = styled.div`
+    background :url(${UMC});
+    width :100%;
+    height: 300px;
+    background-repeat: no-repeat;
+    margin:0px;
+`
+
+function Ad({Show}){
+
+    return Show ? <Img/> : null
+    
+}
+
+export default Ad;
